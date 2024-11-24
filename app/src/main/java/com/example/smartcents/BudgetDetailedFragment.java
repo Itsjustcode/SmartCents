941c8ad15b67ea42a49e756435844764efdf9452
FlameFawn <BASchafer@student.fullsail.edu>
2024-11-23 19:37:05 -0500

    Detailed budget fragment created and nav button for "at a glance" view connected

--- app/src/main/java/com/example/smartcents/BudgetDetailedFragment.java
+++ app/src/main/java/com/example/smartcents/BudgetDetailedFragment.java
@@ -9,24 +9,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class BudgetGlanceFragment extends Fragment {
+public class BudgetDetailedFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //create the layout for this screen.
         return inflater.inflate(R.layout.fragment_budget_glance, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        //Button to navigate to the detailed budget view.
-        view.findViewById(R.id.btn_detailed_view).setOnClickListener(v -> {
-            //Navigate from the "At a glance" view to the detailed view.
-            NavHostFragment.findNavController(this)
-                    .navigate(R.id.action_budgetGlanceFragment_to_budgetDetailedFragment);
-        });
     }
 }
